a83b0200e06fc8e7bb4eaae6f02a6a05bd333e6c
Amanda Enstad <amandaenstad@gmail.com>
2020-11-29 12:45:02 -0600

    form is now saving inputs

--- static/js/results-json.js
+++ static/js/results-json.js
@@ -9,30 +9,52 @@ function getNumberFromCounter(id) {
     return document.getElementById(id).valueAsNumber;
 }
 
-// Dropdown
-function getValueFromDropdown(id) {
-    return document.getElementById(id).value;
+// Credit History Dropdown
+function getCreditValueFromDropdown() {
+    return document.getElementById("creditDropdown").value;
 }
 
 // Input fields
+function inputFields(id) {
+    return document.getElementById(id).value;
+}
 
+// Term of Loan Dropdown
+function getLoanValueFromDropdown() {
+    return document.getElementById("termofLoan").value;
+}
+
+// Home Location Radio Buttons
+function getHomeLocation() {
+    if (document.getElementById("urbanPropertyAreaButton").checked === true) {
+        return "urban";
+    }
+
+    else {
+        return "suburban";
+    }
+
+}
 
 
 function getData()
 {
 
     return {
+        // tab 1 responses
         genderF: isRadioButtonChecked("femaleRadioButton"),
         marriageY: isRadioButtonChecked("yesMarriageButton"),
         selfEmployedY: isRadioButtonChecked("yesSelfEmployedButton"),
         graduatedY: isRadioButtonChecked("graduateButton"),
-
         numberDependents: getNumberFromCounter("dependentCounter"),
-            
+        creditHistory: getCreditValueFromDropdown(),
+        totalIncome: inputFields("totalIncomeInput"),
         
-        // creditScore: getValueFromDropdown("creditDropdown"),
-    };
+        // tab 2 responses
+        homeLocation: getHomeLocation(),
+        loanAmount: inputFields("loanAmountInput"),
+        creditHistory: getLoanValueFromDropdown(),
 
 
-
-}
+    }
+};
